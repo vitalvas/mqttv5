ffccbfee4ea3bc2f3879754824711202ef85647d
Vitaliy Vasylenko <me@vitalvas.com>
2026-01-03 17:28:20 -0500

    refactor: remove unused BidirectionalFlowController
    FlowController is still used for MQTT v5.0 Section 4.9 flow control.
    The bidirectional wrapper was never integrated into client/server code.

--- flow_control_test.go
+++ flow_control_test.go
@@ -116,78 +116,6 @@ func TestFlowControllerConcurrency(t *testing.T) {
 	assert.Equal(t, uint16(0), fc.InFlight())
 }
 
-func TestBidirectionalFlowController(t *testing.T) {
-	t.Run("initial state", func(t *testing.T) {
-		bfc := NewBidirectionalFlowController(10, 20)
-
-		assert.Equal(t, uint16(10), bfc.Send().ReceiveMaximum())
-		assert.Equal(t, uint16(20), bfc.Receive().ReceiveMaximum())
-		assert.True(t, bfc.CanSend())
-		assert.True(t, bfc.CanReceive())
-	})
-
-	t.Run("send flow", func(t *testing.T) {
-		bfc := NewBidirectionalFlowController(3, 5)
-
-		err := bfc.AcquireSend()
-		require.NoError(t, err)
-		assert.Equal(t, uint16(1), bfc.Send().InFlight())
-
-		err = bfc.AcquireSend()
-		require.NoError(t, err)
-
-		err = bfc.AcquireSend()
-		require.NoError(t, err)
-		assert.False(t, bfc.CanSend())
-
-		err = bfc.AcquireSend()
-		assert.ErrorIs(t, err, ErrQuotaExceeded)
-
-		bfc.ReleaseSend()
-		assert.True(t, bfc.CanSend())
-	})
-
-	t.Run("receive flow", func(t *testing.T) {
-		bfc := NewBidirectionalFlowController(5, 3)
-
-		err := bfc.AcquireReceive()
-		require.NoError(t, err)
-		assert.Equal(t, uint16(1), bfc.Receive().InFlight())
-
-		err = bfc.AcquireReceive()
-		require.NoError(t, err)
-
-		err = bfc.AcquireReceive()
-		require.NoError(t, err)
-		assert.False(t, bfc.CanReceive())
-
-		bfc.ReleaseReceive()
-		assert.True(t, bfc.CanReceive())
-	})
-
-	t.Run("update maximums", func(t *testing.T) {
-		bfc := NewBidirectionalFlowController(10, 10)
-
-		bfc.UpdateSendMaximum(5)
-		assert.Equal(t, uint16(5), bfc.Send().ReceiveMaximum())
-
-		bfc.UpdateReceiveMaximum(15)
-		assert.Equal(t, uint16(15), bfc.Receive().ReceiveMaximum())
-	})
-
-	t.Run("reset", func(t *testing.T) {
-		bfc := NewBidirectionalFlowController(10, 10)
-
-		bfc.AcquireSend()
-		bfc.AcquireSend()
-		bfc.AcquireReceive()
-
-		bfc.Reset()
-		assert.Equal(t, uint16(0), bfc.Send().InFlight())
-		assert.Equal(t, uint16(0), bfc.Receive().InFlight())
-	})
-}
-
 func BenchmarkFlowControllerAcquireRelease(b *testing.B) {
 	fc := NewFlowController(65535)
 
@@ -210,18 +138,6 @@ func BenchmarkFlowControllerTryAcquire(b *testing.B) {
 		if fc.TryAcquire() {
 			fc.Release()
 		}
-	}
-}
-
-func BenchmarkBidirectionalFlowController(b *testing.B) {
-	bfc := NewBidirectionalFlowController(65535, 65535)
-
-	b.ResetTimer()
-	b.ReportAllocs()
-
-	for b.Loop() {
-		bfc.AcquireSend()
-		bfc.ReleaseSend()
 	}
 }
 
